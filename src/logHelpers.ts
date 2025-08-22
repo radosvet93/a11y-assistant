0ca7bda3b4bf9b77083cef97e6317e4c30299978
Radosvet Petrov <93radosvet@gmail.com>
2025-08-22 22:05:33 +0100

    removed cheerio implementation

--- src/logHelpers.ts
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-
-export const log = console.log;
-export const error = chalk.bold.red;
-export const success = chalk.green;
-export const warning = chalk.yellow;
-export const info = chalk.cyan;
-export const highlight = chalk.bold;
